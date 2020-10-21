38b63223432046fcf6d1ad6ff8637f17357d8d43
maksktl <matantsev0@gmail.com>
2020-10-21 18:49:00 +0300

    unique link generated partially completed

--- autoAttendanceMonitoring/models.py
+++ autoAttendanceMonitoring/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Students(models.Model):
+    id = models.CharField(max_length=50, primary_key=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
