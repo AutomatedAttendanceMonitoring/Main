9515624c4fa3e8be9c641eefd5b9ae8fca006aa9
maksktl <matantsev0@gmail.com>
2020-10-27 19:10:30 +0300

    urls fix

--- autoAttendanceMonitoring/urls.py
+++ autoAttendanceMonitoring/urls.py
@@ -4,5 +4,5 @@ from autoAttendanceMonitoring import views
 
 urlpatterns = [
     url(r'^$', views.index, name='home'),
-    url(r'^markattendance/', views.mark_student, name='mark_student')
+    # url(r'^markattendance/', views.mark_student, name='mark_student')
 ]
