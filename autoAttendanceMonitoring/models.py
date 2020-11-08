aa0c1748ac8cd381f5bd8afaff962448c1428229
maksktl <matantsev0@gmail.com>
2020-11-08 17:55:04 +0300

    remote db connected

--- autoAttendanceMonitoring/models.py
+++ autoAttendanceMonitoring/models.py
@@ -3,37 +3,41 @@ import uuid
 
 
 # Create your models here.
-class year_of_education(model.Model):
+class YearOfEducation(models.Model):
     number = models.CharField(primary_key=True, max_length=10)
 
-class students(models.Model):
+
+class Student(models.Model):
     email = models.CharField(max_length=50, primary_key=True)
     FName = models.CharField(max_length=50)
     LName = models.CharField(max_length=50)
-    year_of_education = models.ForeignKey('year_of_education')
-    
+    year_of_education = models.ForeignKey(YearOfEducation, models.DO_NOTHING)
 
-class subjects(models.Model):
+
+class Subject(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     name = models.CharField(max_length=30)
-    year = models.ForeignKey('year_of_education')
-    
+    year = models.ForeignKey(YearOfEducation, models.DO_NOTHING)
 
-class lessons(models.Model):
+
+class Lesson(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     kind = models.CharField(max_length=10)
-    subject = models.ForeignKey('subjects')
-    
-class is_taught_by(models.Model):
-    subject = models.ForeignKey('subjects')
-    instructor = models.ForeignKey('lessons')
-    
-class is_present(models.Model):
-    student = models.ForeignKey('students')
-    lesson = models.ForeignKey('lessons')
-    
+    subject = models.ForeignKey(Subject, models.DO_NOTHING)
+
+
+class IsTaughtBy(models.Model):
+    subject = models.ForeignKey(Subject, models.DO_NOTHING)
+    instructor = models.ForeignKey(Lesson, models.DO_NOTHING)
+
+
+class IsPresent(models.Model):
+    student = models.ForeignKey(Student, models.DO_NOTHING)
+    lesson = models.ForeignKey(Lesson, models.DO_NOTHING)
+
+
 class AttendanceLink(models.Model):
     link_parameter = models.CharField(max_length=50, primary_key=True)
     student_email = models.CharField(max_length=256)
